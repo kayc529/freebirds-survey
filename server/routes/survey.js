a18997811e47a8761a4847d1e9b89e5704f6468c
Kay <kaicipher966@gmail.com>
2022-07-18 11:45:39 -0400

    completed do-survey and survey-results features

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -8,6 +8,9 @@ const {
   displayEditSurvey,
   processEditSurvey,
   processDeleteSurvey,
+  displayDoSurvey,
+  processDoSurvey,
+  displaySurveyResults,
 } = require('../controllers/surveyController');
 
 router.get('/', displaySurveyList);
@@ -17,5 +20,9 @@ router.route('/add').get(displayAddSurvey).post(processAddSurvey);
 router.route('/edit/:id').get(displayEditSurvey).post(processEditSurvey);
 
 router.get('/delete/:id', processDeleteSurvey);
+
+router.route('/do-survey/:id').get(displayDoSurvey).post(processDoSurvey);
+
+router.get('/survey-results/:id', displaySurveyResults);
 
 module.exports = router;

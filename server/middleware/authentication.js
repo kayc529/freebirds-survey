b6a293933fdf319aacdbd2b2572ead25e557d0db
Kay <kaicipher966@gmail.com>
2022-07-27 11:39:48 -0400

    remove duplicated files

--- server/middleware/authentication.js
+++ server/middleware/authentication.js
@@ -3,20 +3,20 @@ const { isTokenValid } = require('../utils/jwt');
 const requireAuth = (req, res, next) => {
   console.log('auth required');
   //check if the user has the auth header
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(403).json({ success: false, msg: 'Not authorized' });
-  }
+  //   const authHeader = req.headers.authorization;
+  //   if (!authHeader) {
+  //     return res.status(403).json({ success: false, msg: 'Not authorized' });
+  //   }
 
-  const accessToken = authHeader.split(' ')[1];
-  const user = isTokenValid(accessToken);
+  //   const accessToken = authHeader.split(' ')[1];
+  //   const user = isTokenValid(accessToken);
 
-  if (!user) {
-    return res.status(403).json({ success: false, msg: 'Not authorized' });
-  }
+  //   if (!user) {
+  //     return res.status(403).json({ success: false, msg: 'Not authorized' });
+  //   }
 
-  console.log('auth passed!');
-  req.user = user;
+  //   console.log('auth passed!');
+  //   req.user = user;
   next();
 };
 

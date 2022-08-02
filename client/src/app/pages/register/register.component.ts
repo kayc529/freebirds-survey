efa7b5d6608ed27767d3b4b821b462d38d5a8af1
Kay <kaicipher966@gmail.com>
2022-08-02 13:31:11 -0400

    update login and register pages

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -6,64 +6,56 @@ import { UserRepository } from 'src/app/models/user.repository';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   constructor(
     private route: ActivatedRoute,
     private repository: UserRepository,
     private router: Router
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   register(): void {
     //get the form element
     let registerForm = document.getElementById('register-form');
     let existingID: User[] = [];
 
-
-    const firstName = (<HTMLInputElement>document.getElementById('input-firstname'))?.value || '';
-    const lastName = (<HTMLInputElement>document.getElementById('input-lastname'))?.value || '';
-    const id = (<HTMLInputElement>document.getElementById('input-id'))?.value || '';
-    const username = (<HTMLInputElement>document.getElementById('input-username'))?.value || '';
-    const password = (<HTMLInputElement>document.getElementById('input-password'))?.value || '';
-    const email = (<HTMLInputElement>document.getElementById('input-email'))?.value || '';
-    
+    const firstName =
+      (<HTMLInputElement>document.getElementById('input-firstname'))?.value ||
+      '';
+    const lastName =
+      (<HTMLInputElement>document.getElementById('input-lastname'))?.value ||
+      '';
+    const username =
+      (<HTMLInputElement>document.getElementById('input-username'))?.value ||
+      '';
+    const password =
+      (<HTMLInputElement>document.getElementById('input-password'))?.value ||
+      '';
+    const email =
+      (<HTMLInputElement>document.getElementById('input-email'))?.value || '';
 
     //check the values are filled
-    if (!firstName || !lastName || !id || !username || !password || !email) {
+    if (!firstName || !lastName || !username || !password || !email) {
       alert('Please fill in all the info');
       return;
     }
 
-    for (let i = 0; i < existingID.length; i++) {
-      if (id == existingID[i]) {
-        alert('User ID already exists!');
-        return;
-      }
-    }
-
-    const registerUser: User = {id: '', username, password, email, firstName, lastName};
-
-    // this.repository.registerUser(registerUser).subscribe({
-    //   next: (data) => {
-    //     alert('Registered!');
-    //   },
-    //   error: (error) => {
-    //     console.log(error);
-    //     const msg = error.error.msg || 'Failed to register';
-    //     alert(msg);
-    //   }
-    // }
-    // );
+    const registerUser: User = {
+      id: '',
+      username,
+      password,
+      email,
+      firstName,
+      lastName,
+    };
 
     this.repository.registerUser(registerUser).subscribe(
       (data: any) => {
         alert('Registered!');
-        location.href='/login'
+        location.href = '/login';
       },
       (err: any) => {
         console.log(err);
